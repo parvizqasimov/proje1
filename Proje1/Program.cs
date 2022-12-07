1a14bc1e701f1ca72eb5e1b4aa524f2711172943
parvizqasimov <119752037+parvizqasimov@users.noreply.github.com>
2022-12-07 23:36:42 +0400

    first commit proje1

---
+++ Proje1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Proje1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("eded 1 i daxil edin");
+            int eded1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("eded 2 ni daxil edin");
+            int eded2 = Convert.ToInt32(Console.ReadLine());
+
+            int cem = eded1 + eded2;
+            Console.WriteLine(" cavab = " + cem);
+
+        }
+    }
+}
